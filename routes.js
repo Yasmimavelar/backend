53dbc8e49c357fc68a5cd3bba4a654f0e1427bbd
DAniel Vitor <danielvitor@usp.br>
2020-01-26 22:49:46 -0300

    test

--- routes.js
+++ routes.js
@@ -48,9 +48,9 @@ io.on('connection', socket => {
   });
 
   socket.on('updateTarefas', object => {
-    connection.query("update chart set lineMecanica = "+ object.lineMecanica +" where dia = date(now());", () => {
+    /*connection.query("update chart set lineMecanica = "+ object.lineMecanica +" where dia = date(now());", () => {
       socket.broadcast.emit('changeChart')
-    });
+    }); */
     connection.query("update tarefas set checkin = ("+ object.value +") where tarefa = '" + object.tarefa + "';", () => {
       socket.broadcast.emit('changeTarefas');
     });
