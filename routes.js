cfc7a6aa576caa0a6c73af84a9e50ca8b810d5a1
DAniel Vitor <danielvitor@usp.br>
2020-02-11 12:15:56 -0300

    update back

--- routes.js
+++ routes.js
@@ -1,4 +1,3 @@
-// app.js
 var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -9,10 +8,7 @@ var app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-
 require('dotenv/config');
-
-
 var connection = mysql.createPool({
     host: process.env.HOST, 
     user: process.env.USER, 
@@ -23,13 +19,28 @@ var connection = mysql.createPool({
 
 app.use(cors());
 app.use(bodyParser());
-// API TAREFAS
 
-// socket.io
 io.on('connection', socket => {
 
-  console.log('connected')
-
+  //TAREFAS
+  socket.on('initialCheckin', () => {
+    connection.query('select categoria, count(checkin) as checkin from tarefas where checkin = true group by categoria', function (error, results, fields) {
+      if (error) throw error;
+      socket.emit('getCheckin', results);
+    });
+  });
+  socket.on('updateTarefas', object => {
+    connection.query("update tarefas set checkin = ("+ object.value +") where tarefa = '" + object.tarefa + "';", () => {
+      socket.broadcast.emit('changeTarefas');
+      socket.broadcast.emit('changeCheckin');
+    });
+  });
+  socket.on('initialTarefas', () => {
+    connection.query('SELECT HIGH_PRIORITY * FROM tarefas where categoria = "Mecanica" ORDER BY prazo', function (error, results, fields) {
+      if (error) throw error;
+      socket.emit('getTarefas', results);
+    });
+  });
   //CHART
   socket.on('initialChart', () => {
     connection.query("insert into chart (dia) values (date(now()))");
@@ -37,36 +48,12 @@ io.on('connection', socket => {
       socket.emit('getChart', row)
     });
   });
+
   socket.on('updateChart', lineMecanica => {
     connection.query("update chart set lineMecanica = "+ lineMecanica +" where dia = date(now());", () => {
       socket.broadcast.emit('changeChart')
     });
-
-    
   });
-
-  //TAREFAS
-  socket.on('initialTarefas', () => {
-    connection.query('SELECT HIGH_PRIORITY * FROM tarefas where categoria = "Mecanica" ORDER BY prazo', function (error, results, fields) {
-      if (error) throw error;
-      socket.emit('getTarefas', results);
-    });
-  });
-
-  socket.on('initialCheckin', () => {
-    connection.query('select categoria, count(checkin) as checkin from tarefas where checkin = true group by categoria', function (error, results, fields) {
-      if (error) throw error;
-      socket.emit('getCheckin', results);
-    });
-  });
-
-  socket.on('updateTarefas', object => {
-    connection.query("update tarefas set checkin = ("+ object.value +") where tarefa = '" + object.tarefa + "';", () => {
-      socket.broadcast.emit('changeTarefas');
-      socket.broadcast.emit('changeCheckin');
-    });
-  })
-
   //COMENTARIOS
   socket.on('initialCom', () => {
     connection.query("SELECT * FROM comentarios", function(err, results) {
@@ -74,31 +61,23 @@ io.on('connection', socket => {
       socket.emit('getCom',results)
     });
   });
-
-
   socket.on('markDone', com => {
-    connection.query("INSERT INTO comentarios VALUES ('" + com + "')", () => {
+    connection.query("INSERT INTO comentarios (comentario) VALUES ('" + com + "')", () => {
         socket.broadcast.emit('changeCom')
     });
   });
-
-  // Eventos
-
+  // EVENTOS
   socket.on('initialEvent', () => {
     connection.query('SELECT * FROM eventos', (err, results) => {
       if (err) throw err;
       socket.emit('getEvento', results)
     })
   })
-
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-
-
 });
 
-// Iniciando o servidor.
 server.listen(port, () => {
   console.log('API subida com sucesso!');
  }); 
